73771fa3dd366c5d4252d201dfdb9a8a1b4406db
untitled97 <125086986+untitled97@users.noreply.github.com>
2023-02-27 01:43:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,84 +5,84 @@
 
 bool checkPrime(uint64_t value) {
 if (value > 1) {
-        uint64_t b, a = 0;
-        for (uint64_t i = 1; i <= value; i++) {
-                b = value;
-			b %= i;
-			if (b == 0)
-				a++;
-		}
-		if (a == 2)
-			return true;
-		else
-			return false;
+uint64_t b, a = 0;
+for (uint64_t i = 1; i <= value; i++) {
+	b = value;
+		b %= i;
+		if (b == 0)
+			a++;
 	}
-	return 0;
+	if (a == 2)
+		return true;
+	else
+		return false;
+}
+return 0;
 }
 
 uint64_t nPrime(uint64_t n) {
-	if (n >= 1) {
-		uint64_t prime = 2, b1, number = 1;
-		while (number <= n) {
-			uint64_t a = 0;
-			for (uint64_t i = 1; i <= prime; i++) {
-				b1 = prime;
-				b1 %= i;
-				if (b1 == 0)
-					a++;
-			}
-			if (a == 2 && number < n) {
-				prime++;
-				number++;
-			} else if (a == 2 && number == n) {
-				return prime;
-			} else
-				prime++;
+if (n >= 1) {
+	uint64_t prime = 2, b1, number = 1;
+	while (number <= n) {
+		uint64_t a = 0;
+		for (uint64_t i = 1; i <= prime; i++) {
+			b1 = prime;
+			b1 %= i;
+			if (b1 == 0)
+				a++;
 		}
+		if (a == 2 && number < n) {
+			prime++;
+			number++;
+		} else if (a == 2 && number == n) {
+			return prime;
+		} else
+			prime++;
 	}
-	return 0;
+}
+return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	if (value >= 1) {
-		uint64_t a2 = 0, b2;
-		while (1) {
-			value++;
-			for (uint64_t i = 1; i <= value; i++) {
-				b2 = value;
-				b2 %= i;
-				if (b2 == 0)
-					a2++;
-			}
-			if (a2 == 2)
-				return value;
-			else
-				a2 = 0;
+if (value >= 1) {
+	uint64_t a2 = 0, b2;
+	while (1) {
+		value++;
+		for (uint64_t i = 1; i <= value; i++) {
+			b2 = value;
+			b2 %= i;
+			if (b2 == 0)
+				a2++;
 		}
+		if (a2 == 2)
+			return value;
+		else
+			a2 = 0;
 	}
-	return 0;
+}
+return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	if (hbound > 1) {
-		uint64_t sum = 0, num = 1, a3 = 0, b3, s_prime = 2;
-		while (num < hbound) {
-			for (uint64_t i = 1; i <= s_prime; i++) {
-				b3 = s_prime;
-				b3 %= i;
-				if (b3 == 0)
-					a3++;
-			} 
-			if (a3 == 2) {
-				sum += s_prime;
-				num++;
-				s_prime++;
-			} else {
-				s_prime++;
-			}
-			a3 = 0;
+if (hbound > 1) {
+	uint64_t sum = 0, num = 1, a3 = 0, b3, s_prime = 2;
+	while (num < hbound) {
+		for (uint64_t i = 1; i <= s_prime; i++) {
+			b3 = s_prime;
+			b3 %= i;
+			if (b3 == 0)
+				a3++;
+		} 
+		if (a3 == 2) {
+			sum += s_prime;
+			num++;
+			s_prime++;
+		} else {
+			s_prime++;
 		}
-		return sum;
+		a3 = 0;
 	}
-	return 0;
+	return sum;
+}
+return 0;
 }
